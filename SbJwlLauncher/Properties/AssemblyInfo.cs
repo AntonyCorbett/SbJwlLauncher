2e76715a97c789b4ff00081ae8603825fb515208
Antony Corbett <antony@corbetts.org.uk>
2023-11-24 15:43:57 +0000

    bump ver

--- SbJwlLauncher/Properties/AssemblyInfo.cs
+++ SbJwlLauncher/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SbJwlLauncher")]
-[assembly: AssemblyCopyright("Copyright © 2019, 2020")]
+[assembly: AssemblyCopyright("Copyright © 2019, 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -14,4 +14,4 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("28c163a5-18f5-43e3-ae97-cb8025cc8e94")]
 
-[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyVersion("1.0.0.5")]
